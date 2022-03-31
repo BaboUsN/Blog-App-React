e8892c2a558c0da733b5a09e53681aa75340e38d
Burak <burakeyuksel@gmail.com>
2022-03-31 04:29:05 +0300

    added useFetch hook with useful usage

--- src/customhooks/useFetch.js
+++ src/customhooks/useFetch.js
@@ -1,11 +1,39 @@
 import { useEffect, useState } from "react";
-const useFetch = (url) => {
+const useFetch = (url, fetchType, fetchData) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
+  let postPar;
+  if (fetchType.toLowerCase())
+    switch (fetchType.toLowerCase()) {
+      case "get":
+        postPar = null;
+        break;
+      case "post":
+        postPar = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(fetchData),
+        };
+        break;
+      case "put":
+        postPar = {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(fetchData),
+        };
+        break;
+      case "delete":
+        postPar = { method: "DELETE" };
+        break;
+      default:
+        console.err(new Error("dsada"));
+    }
+
   useEffect(() => {
-    fetch("http://localhost:3000" + url)
+    const abortCont = new AbortController();
+    fetch("http://localhost:3000" + url, { signal: abortCont.signal }, postPar)
       .then((res) => {
         if (!res.ok) {
           throw new Error("There is an Error");
@@ -18,8 +46,12 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === "AbortError") {
+          console.log("Abort Error");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
   }, [url]);
   return { data, isPending, error };
